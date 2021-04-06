7dc3a08a72f2bb8b50b53551687d1b5932f39edf
tzvivi <tzdovi@gmail.com>
2021-04-06 10:31:05 +0300

    added find_max function

--- hacker__news.py
+++ hacker__news.py
@@ -1,7 +1,6 @@
 import json
 from urllib.request import urlopen
 
-
 HACKER_PATH = "https://hacker-news.firebaseio.com/v0/item/"
 KIDS = "kids"
 TEXT = "text"
@@ -20,7 +19,25 @@ def get_kids(json_file):
 	return kids_text
 
 
+def find_max(lower=1e7, upper=1e8, found_upper=False):
+	if not found_upper:
+		if get_json(int(upper)) is None:
+			return find_max(lower, upper, True)
+		return find_max(lower, upper * 2)
+	else:
+		if upper - lower > 1:
+			new_bound = int((lower + upper) / 2)
+			if get_json(new_bound) is None:
+				return find_max(lower, new_bound, found_upper=True)
+			return find_max(new_bound, upper, found_upper=True)
+		return upper
+
+
 if __name__ == "__main__":
-	data = get_json(2921506)
-	print(data["kids"])
-	print(get_kids(data))
+	print(f"found: {find_max()}")
+	# data = get_json(99999224)
+	# print(data)
+	# if data is not None:
+	# 	if KIDS in data:
+	# 		print(data[KIDS])
+	# 		print(get_kids(data))
